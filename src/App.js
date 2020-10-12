6abe0f1c5a0c8b9301185bae54695ee4f57ec8d8
tabrezakhtar <tabrez.akhtar1@gmail.com>
2020-10-12 18:36:46 +0100

    use grid layout for common web app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ function App() {
     <main>
 
       <div className="wrapper">
-        <div>header</div>
-        <div>2</div>
+        <div className="header">header</div>
+        <div className="item2">2</div>
         <div className="item3">3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
+        <div className="footer">footer</div>
       </div>
 
     </main>
