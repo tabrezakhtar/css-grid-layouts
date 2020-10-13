265e59ceb3dc411394de0df5e08835a8aef38242
tabrezakhtar <tabrez.akhtar1@gmail.com>
2020-10-13 17:06:28 +0100

    messing around with grid

--- src/App.js
+++ src/App.js
@@ -5,9 +5,37 @@ function App() {
     <main>
 
       <div className="wrapper">
-        <div className="header">header</div>
-        <div className="item2">2</div>
-        <div className="item3">3</div>
+        <div className="header">
+          <span className="logo">header</span>
+          <ul className={'header__menu'}>
+            <li className="menu__item"><a href="">Home</a></li>
+            <li className="menu__item"><a href="">About</a></li>
+            <li className="menu__item"><a href="">About2</a></li>
+            <li className="menu__item"><a href="">About3</a></li>
+          </ul>
+        </div>
+        <div className="item2">This is a sidebar</div>
+        <div className="item3">
+        The natural environment of Java is tropical rainforest, with ecosystems ranging from coastal mangrove forest on the north coast, rocky coastal cliffs on the southern coast, and low-lying tropical forest to high altitude rainforest on the slopes of mountainous volcanic regions in the interior. The Javan environment and climate gradually alters from west to east; from wet and humid dense rainforest in western parts, to a dry savanna environment in the east, corresponding to the climate and rainfall in these regions.
+
+Originally Javan wildlife supported a rich biodiversity, where numbers of endemic species of flora and fauna flourished; such as the Javan rhinoceros, There are also several endemic amphibian species in Java, including 5 species of tree frogs.
+
+Fossilised remains of Homo erectus, popularly known as the "Java Man", dating back 1.7 million years were found along the banks of the Bengawan Solo River.
+
+The emergence of civilization on the island of Java is often associated with the story of Aji Saka from 78 AD. Although Aji Saka is said to be the bearer of civilization on Java, the story received several objections and rebuttals from other historical sources. Valmiki's Ramayana, made around 500 BC, records that Java already had a governmental organization long before the story:
+
+"Yawadwipa is decorated with seven kingdoms, gold and silver islands, rich in gold mines, and there is Cicira (cold) Mountain that touches the sky with its peak."(p6)
+
+The story of Aji Saka is a Neo Javanese story. This story has not yet been found to be relevant in the Old Javanese text. This story tells of events in the Medang Kamulan kingdom in Java in the past. At that time, the king of Medang Kamulan Prabu Dewata Cengkar was replaced by Aji Saka. This story is considered as an allegory of the entry of Indians into Java. Referring to the Liang dynasty information, the Javanese kingdom was divided into two: the pre-Hinduism kingdom and the post-Hindu kingdom, which began in 78 AD.(p5 and 7)
+
+Islam became the dominant religion in Java at the end of the 16th century. During this era, the Islamic kingdoms of Demak, Cirebon, and Banten were ascendant. The Mataram Sultanate became the dominant power of central and eastern Java at the end of the 16th century. The principalities of Surabaya and Cirebon were eventually subjugated such that only Mataram and Banten were left to face the Dutch in the 17th century.
+
+Java's major role during the early part of the colonial period was as a producer of rice. In spice producing islands like Banda, rice was regularly imported from Java, to supply the deficiency in means of subsistence.
+
+The advent of trucks and railways where there had previously only been buffalo and carts, telegraph systems, and more coordinated distribution systems under the colonial government all contributed to famine elimination in Java, and in turn, population growth. There were no significant famines in Java from the 1840s through to the Japanese occupation in the 1940s. However, other sources claimed the Dutch's Cultivation system is linked to famines and epidemics in the 1840s, firstly in Cirebon and then Central Java, as cash crops such as indigo and sugar had to be grown instead of rice.
+
+Indonesian nationalism first took hold in Java in the early 20th century, and the struggle to secure the country's independence following World War II was centered in Java. In 1949, Indonesian independence was recognised.
+        </div>
         <div className="footer">footer</div>
       </div>
 
